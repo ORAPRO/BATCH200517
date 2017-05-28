52e63e90a3d6dbb78b0ed57858175f5c37de3787
svaduka <sainagaraju.vaduka@gmail.com>
2017-05-28 14:50:56 +0530

    Added HDFSService Code

---
+++ HDFSService/src/com/laboros/HDFSService.java
@@ -0,0 +1,17 @@
+package com.laboros;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HDFSService extends Configured implements Tool {
+
+	
+	public static void main(String[] args) {
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+		return 0;
+	}
+
+}
