0c756f038a876ed82574f221584ee51c3c74d245
svaduka <sainagaraju.vaduka@gmail.com>
2017-09-02 15:04:48 +0530

    Added advanced MapReduce Programs

---
+++ Batch200517/src/MyDC.java
@@ -0,0 +1,99 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MyDC {
+	
+	
+	public static class MyMapper extends Mapper<LongWritable,Text, Text, Text> {
+        
+		
+		private Map<String, String> abMap = new HashMap<String, String>();
+				private Text outputKey = new Text();
+				private Text outputValue = new Text();
+		
+		protected void setup(Context context) throws java.io.IOException, InterruptedException{
+			Path[] files = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+			
+			
+			for (Path p : files) {
+				if (p.getName().equals("abc.dat")) {
+					BufferedReader reader = new BufferedReader(new FileReader(p.toString()));
+					String line = reader.readLine();
+					while(line != null) {
+						String[] tokens = line.split("\t");
+						String ab = tokens[0];
+						String state = tokens[1];
+						abMap.put(ab, state);
+						line = reader.readLine();
+					}
+				}
+			}
+			if (abMap.isEmpty()) {
+				throw new IOException("Unable to load Abbrevation data.");
+			}
+		}
+
+		
+        protected void map(LongWritable key, Text value, Context context)
+            throws java.io.IOException, InterruptedException {
+        	
+        	
+        	String row = value.toString(); //up	199654321
+        	String[] tokens = row.split("\t");
+        	//tokens[0] = up
+        	//tokens[1] = 199654321
+        	String inab = tokens[0];//inab = up
+        	String state = abMap.get(inab); //state = Uttar_pradesh
+        	outputKey.set(state);
+        	outputValue.set(row);
+      	  	context.write(outputKey,outputValue);
+        }  
+}
+	
+	
+  public static void main(String[] args) 
+                  throws IOException, ClassNotFoundException, InterruptedException {
+    
+    Job job = new Job();
+    job.setJarByClass(MyDC.class);
+    job.setJobName("DCTest");
+    job.setNumReduceTasks(0);
+    
+    try{
+    DistributedCache.addCacheFile(new URI("/user/edureka/ADMR/abc.dat"), job.getConfiguration());
+    }catch(Exception e){
+    	System.out.println(e);
+    }
+    
+    job.setMapperClass(MyMapper.class);
+    
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(Text.class);
+    
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.waitForCompletion(true);
+    
+    
+  }
+}
